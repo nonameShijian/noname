8c89a6e6c022f56d79fb7a20a38e6304e087e475
shijian <2954700422@qq.com>
2022-04-23 02:42:03 +0800

    update.js fix, 增加备注

--- update.js
+++ update.js
@@ -1,58 +1,103 @@
 window.updateList = [
-	//app文件夹
-	'app/index.js',
-	'app/menu.js',
-	'app/redirect.js',
-	
-	'downloadExtension.html',
-	'main.js',
-	'package.json',
-	//'package-lock.json',
-	'update.html',
-	
-	//教程
-	'(必看)无名杀全教程9.9.pdf',
-	
-	//应用配置
-	'extension/应用配置/extension.js',
-	
-	//在线更新
-	'extension/在线更新/extension.js',
-	
-	//拖拽读取
-	'extension/拖拽读取/extension.js',
-	'extension/拖拽读取/extension.css',
-	'extension/拖拽读取/updateContent',
-	'extension/拖拽读取/version',
-	'extension/拖拽读取/node_modules/.bin/7z-wasm',
-	'extension/拖拽读取/node_modules/.bin/7z-wasm.cmd',
-	'extension/拖拽读取/node_modules/.bin/7z-wasm.ps1',
-	'extension/拖拽读取/node_modules/7z-wasm/bin/cli',
-	'extension/拖拽读取/node_modules/7z-wasm/7zz.es6.js',
-	'extension/拖拽读取/node_modules/7z-wasm/7zz.umd.js',
-	'extension/拖拽读取/node_modules/7z-wasm/7zz.wasm',
-	'extension/拖拽读取/node_modules/7z-wasm/cli.js',
-	'extension/拖拽读取/node_modules/7z-wasm/index.d.ts',
-	'extension/拖拽读取/node_modules/7z-wasm/License.txt',
-	'extension/拖拽读取/node_modules/7z-wasm/package.json',
-	'extension/拖拽读取/node_modules/7z-wasm/README.md',
-	'extension/拖拽读取/node_modules/7z-wasm/unRarLicense.txt',
-	'extension/拖拽读取/node_modules/readline-sync/LICENSE',
-	'extension/拖拽读取/node_modules/readline-sync/package.json',
-	'extension/拖拽读取/node_modules/readline-sync/README-Deprecated.md',
-	'extension/拖拽读取/node_modules/readline-sync/README.md',
-	'extension/拖拽读取/node_modules/readline-sync/lib/encrypt.js',
-	'extension/拖拽读取/node_modules/readline-sync/lib/read.cs.js',
-	'extension/拖拽读取/node_modules/readline-sync/lib/read.ps1',
-	'extension/拖拽读取/node_modules/readline-sync/lib/read.sh',
-	'extension/拖拽读取/node_modules/readline-sync/lib/readline-sync.js'
+    //app文件夹
+    'app/index.js',
+    'app/menu.js',
+    'app/redirect.js',
+
+    'downloadExtension.html',
+    'main.js',
+    'package.json',
+    //'package-lock.json',
+    'update.html',
+
+    //教程
+    '(必看)无名杀全教程9.9.pdf',
+
+    //应用配置
+    'extension/应用配置/extension.js',
+
+    //在线更新
+    'extension/在线更新/extension.js',
+
+    //拖拽读取
+    'extension/拖拽读取/extension.js',
+    'extension/拖拽读取/extension.css',
+    'extension/拖拽读取/updateContent',
+    'extension/拖拽读取/version',
+    'extension/拖拽读取/node_modules/.bin/7z-wasm',
+    'extension/拖拽读取/node_modules/.bin/7z-wasm.cmd',
+    'extension/拖拽读取/node_modules/.bin/7z-wasm.ps1',
+    'extension/拖拽读取/node_modules/7z-wasm/bin/cli',
+    'extension/拖拽读取/node_modules/7z-wasm/7zz.es6.js',
+    'extension/拖拽读取/node_modules/7z-wasm/7zz.umd.js',
+    'extension/拖拽读取/node_modules/7z-wasm/7zz.wasm',
+    'extension/拖拽读取/node_modules/7z-wasm/cli.js',
+    'extension/拖拽读取/node_modules/7z-wasm/index.d.ts',
+    'extension/拖拽读取/node_modules/7z-wasm/License.txt',
+    'extension/拖拽读取/node_modules/7z-wasm/package.json',
+    'extension/拖拽读取/node_modules/7z-wasm/README.md',
+    'extension/拖拽读取/node_modules/7z-wasm/unRarLicense.txt',
+    'extension/拖拽读取/node_modules/readline-sync/LICENSE',
+    'extension/拖拽读取/node_modules/readline-sync/package.json',
+    'extension/拖拽读取/node_modules/readline-sync/README-Deprecated.md',
+    'extension/拖拽读取/node_modules/readline-sync/README.md',
+    'extension/拖拽读取/node_modules/readline-sync/lib/encrypt.js',
+    'extension/拖拽读取/node_modules/readline-sync/lib/read.cs.js',
+    'extension/拖拽读取/node_modules/readline-sync/lib/read.ps1',
+    'extension/拖拽读取/node_modules/readline-sync/lib/read.sh',
+    'extension/拖拽读取/node_modules/readline-sync/lib/readline-sync.js',
+
+    // typings
+    "typings/ai.d.ts",
+    "typings/AIConfig.d.ts",
+    "typings/ArrayEx.d.ts",
+    "typings/brawl.d.ts",
+    "typings/button.d.ts",
+    "typings/card.d.ts",
+    "typings/CardConfig.d.ts",
+    "typings/CharacterConfig.d.ts",
+    "typings/cheat.d.ts",
+    "typings/click.d.ts",
+    "typings/client.d.ts",
+    "typings/content.d.ts",
+    "typings/control.d.ts",
+    "typings/cordova-plugin-file.d.ts",
+    "typings/cordova.d.ts",
+    "typings/create.d.ts",
+    "typings/DateEx.d.ts",
+    "typings/develop.d.ts",
+    "typings/dialog.d.ts",
+    "typings/electron.d.ts",
+    "typings/event.d.ts",
+    "typings/ExInterface.d.ts",
+    "typings/ExtensionConfig.d.ts",
+    "typings/filter.d.ts",
+    "typings/game.d.ts",
+    "typings/get.d.ts",
+    "typings/HTMLDivElementEx.d.ts",
+    "typings/import-card.d.ts",
+    "typings/import-character.d.ts",
+    "typings/index.d.ts",
+    "typings/init.d.ts",
+    "typings/lib.d.ts",
+    "typings/LocalGameConfigMenu.d.ts",
+    "typings/message.d.ts",
+    "typings/nodews.d.ts",
+    "typings/player.d.ts",
+    "typings/SkillConfig.d.ts",
+    "typings/sort.d.ts",
+    "typings/status.d.ts",
+    "typings/type.d.ts",
+    "typings/ui.d.ts",
+    "typings/windowEx.d.ts",
+    "typings/ws.d.ts"
 ];
 
 window.removeFileList = [
-	//教程
-	'(必看)无名杀全教程9.5.pdf',
-	'(必看)无名杀全教程9.6.pdf',
-	'(必看)无名杀全教程9.7.pdf',
-	'(必看)无名杀全教程9.8.pdf',
-	'(必看)无名杀全教程9.9.pdf'
+    //教程
+    '(必看)无名杀全教程9.5.pdf',
+    '(必看)无名杀全教程9.6.pdf',
+    '(必看)无名杀全教程9.7.pdf',
+    '(必看)无名杀全教程9.8.pdf',
+    '(必看)无名杀全教程9.9.pdf'
 ];
